a7dd0ca28d69f81bebb307b9a0c92ed99ab74437
EliGolam <eligolam.mahfuzul.dev@gmail.com>
2024-06-23 14:52:19 +0200

    Separate Node.js and client-side TypeScript configurations

--- vite.config.ts
+++ vite.config.ts
@@ -13,7 +13,32 @@ export default defineConfig({
   },
   build: {
     rollupOptions: {
-      external: ['fsevents']
+      external: [
+        'fsevents', // Add any other modules you want to externalize
+        'fs',
+        'path',
+        'module',
+        'crypto',
+        'tty',
+        'util',
+        'os',
+        'events',
+        'stream',
+        'zlib',
+        'http',
+        'https',
+        'url',
+        'assert',
+        'buffer',
+        'querystring',
+        'net',
+        'tls',
+        'child_process',
+        'worker_threads',
+        'perf_hooks',
+        'readline',
+        'dns'
+      ]
     }
   }
 })
