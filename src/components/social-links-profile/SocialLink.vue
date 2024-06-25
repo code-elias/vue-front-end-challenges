08638a2da6c033b9f501d1ca278c88a3c2620686
EliGolam <eligolam.mahfuzul.dev@gmail.com>
2024-06-26 00:26:42 +0200

    Added Social Link

---
+++ src/components/social-links-profile/SocialLink.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+defineProps<{
+  linkTitle: string
+  link: string
+}>()
+</script>
+
+<template>
+  <div class="wrapper">
+    <a :href="link" target="_blank" class="link">{{ linkTitle }}</a>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.wrapper {
+  overflow: hidden;
+}
+
+.link {
+  display: block;
+  width: 100%;
+  padding: 0.8rem 0;
+  text-align: center;
+}
+</style>
--- src/views/challenges/SocialLinksProfileView.vue
+++ src/views/challenges/SocialLinksProfileView.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import { onMounted } from 'vue'
+import SocialLink from '../../components/social-links-profile/SocialLink.vue'
 
 onMounted(() => {
   console.log('Mounted Social Links')
@@ -8,10 +9,25 @@ onMounted(() => {
 
 <template>
   <div class="container">
-    <div class="card test">
-      <div class="avatar img-container">
-        <img src="/images/avatar-jessica.jpeg" alt="Profile Picture" class="avatar__img" />
-      </div>
+    <div class="card flex-col-center">
+      <header class="card__header flex-col-center">
+        <div class="avatar img-container">
+          <img src="/images/avatar-jessica.jpeg" alt="Profile Picture" class="avatar__img" />
+        </div>
+
+        <section class="card-title flex-col-center">
+          <h2 class="card-title__name">Elias Golam</h2>
+          <p class="card-title__location">Milan, Italy</p>
+        </section>
+
+        <section class="card-subtitle">
+          <p>Software Developer, UI/UX Designer and Creative Writer</p>
+        </section>
+      </header>
+
+      <section class="social">
+        <SocialLink class="social__link" linkTitle="GitHub" link="https://www.google.com/" />
+      </section>
     </div>
   </div>
 </template>
